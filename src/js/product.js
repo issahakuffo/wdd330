76a1c09de8a8e4be0d1da3a6bcf8aea41f7d71dc
IsaiasPalaco <isaiaspalaco@gmail.com>
2025-03-20 02:51:53 +0200

    updated all quotes with double quote with "npm run format: comand

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
-import { getParam } from './utils.mjs';
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam('product');
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
@@ -18,8 +18,7 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-  const addtoCartbut = document.querySelector("#addToCart");
-  addtoCartbut.addEventListener("click", (event)=>{
-    addToCartHandler(event)
-   });
- 
+const addtoCartbut = document.querySelector("#addToCart");
+addtoCartbut.addEventListener("click", (event) => {
+  addToCartHandler(event);
+});
