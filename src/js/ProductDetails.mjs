223791a044ce41da3884bd384a5b1107baae16f1
desparzas <desparzas7902@gmail.com>
2025-03-20 14:38:41 -0500

    week 1 fix master

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -35,13 +35,13 @@ function productDetailsTemplate(product) {
   document.querySelector("h2").textContent = product.Brand.Name;
   document.querySelector("h3").textContent = product.NameWithoutBrand;
 
-  const productImage = document.getElementById("productImage");
+  const productImage = document.querySelector(".divider img");
   productImage.src = product.Image;
   productImage.alt = product.NameWithoutBrand;
 
-  document.getElementById("productPrice").textContent = product.FinalPrice;
-  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
-  document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector(".product-card__price").textContent = product.FinalPrice;
+  document.querySelector(".product__color").textContent = product.Colors[0].ColorName;
+  document.querySelector(".product__description").innerHTML = product.DescriptionHtmlSimple;
 
   document.getElementById("addToCart").dataset.id = product.Id;
 }
