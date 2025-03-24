02e3197074f295dbc431fc7a792ac5ed7b5464fc
Issah Akuffo <issahkuffo300@gmail.com>
2025-03-24 02:18:45 +0000

    updATE WK3

--- src/js/cart.js
+++ src/js/cart.js
@@ -15,13 +15,14 @@ function cartItemTemplate(item) {
       alt="${item.Name}"
     />
   </a>
+
   <a href="#">
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+</li>`
 
   return newItem;
 }
--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,9 @@ import ProductDetails from "./ProductDetails.mjs";
 loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
+
 const productID = getParam("product");
+
 const product = new ProductDetails(productID, dataSource);
 
 product.init();
