ee0645f3b67f776b9b1b9b77bfc80ec80f1bed15
desparzas <desparzas7902@gmail.com>
2025-03-30 02:43:00 -0500

    week 4

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,10 +1,12 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
+import { alertMessage } from "./utils.mjs";
 
-function convertToJson(res) {
+async function convertToJson(res) {
+  const jsonResponse = await res.json();
   if (res.ok) {
-    return res.json();
+    return jsonResponse;
   } else {
-    throw new Error("Bad Response");
+    throw { name: "servicesError", message: jsonResponse };
   }
 }
 
@@ -20,23 +22,39 @@ export default class ExternalServices {
     return data.Result;
   }
   async checkout(order) {
-    const url = `${baseURL}checkout`;
-    const params = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(order),
+    try {
+      const url = `${baseURL}checkout`;
+      const params = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
+      };
+
+      console.log(url);
+      console.log(params);
+
+      const response = await fetch(url, params);
+
+      console.log(response.status);
+
+      if (response.status === 400) {
+        const errorResponse = await convertToJson(response);
+        throw { name: "servicesError", message: errorResponse };
+      }
+
+      console.log(response);
+      const data = await convertToJson(response);
+      return data;
+    } catch (error) {
+      console.log("Error in checkout", error);
+      if (error.name === "servicesError") {
+        alertMessage(`Error: ${error.message.error || "Something went wrong. Please try again."}`);
+      } else {
+        console.error("Unexpected error:", error);
+      }
     }
-
-    console.log(url);
-    console.log(params);
-
-    const response = await fetch(url, params);
-
-    console.log(response);
-    const data = await convertToJson(response);
-    return data;
   }
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
